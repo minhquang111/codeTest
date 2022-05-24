9ef11b3142b8dd1e8f1016c3e90952d28025e3f8
minhquang111 <minhquang2532000@gmail.com>
2022-05-25 00:37:39 +0700

    up

---
+++ index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const authRouter = require("./routes/auth");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      'mongodb://localhost/test'
+    );
+
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth", authRouter);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
